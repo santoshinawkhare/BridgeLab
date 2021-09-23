f99682c1ebac2859dbecf219c48bd544bb7dfaf6
santoshinawkhare <santoshine1908@gmail.com>
2021-09-23 12:57:33 +0530

    day 3 class work are added

---
+++ Relational_operator.java
@@ -0,0 +1,13 @@
+public class Relational_operator
+{
+ public static void main(String args[])
+ {
+  int var1 = 5;
+  int var2 = 6;
+   if(var1 > var2){
+      System.out.print (var1);
+   } else{
+      System.out.print (var2);
+   } 
+ }
+}
