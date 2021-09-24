130eca3c723be34b57f05acc050b210dda271606
santoshinawkhare <santoshine1908@gmail.com>
2021-09-24 15:10:25 +0530

    day 3 shell program are added

---
+++ Shell.sh/day3Assignment/Even.sh
@@ -0,0 +1,17 @@
+#bin/bash 
+# Sum of first five
+
+arr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+sum=0
+for i in ${arr[@]}
+do
+    if((`expr $i % 2`==0))
+    then
+    {
+     echo $i
+     sum=`expr $sum + $i`
+    }
+    fi
+done
+
+echo $sum
---
+++ Shell.sh/day3Assignment/LastFive.sh
@@ -0,0 +1,11 @@
+#bin/bash 
+# Sum of first five
+
+arr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+for i in ${arr[@]:10:11}
+do
+    echo $i
+    sum=`expr $sum + $i`
+done
+
+echo $sum
